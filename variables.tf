e480c0fab3594b5fb0ea895b222547a6cb7ca521
Weston Bassler <wbassler@mesosphere.com>
2019-03-07 09:47:24 -0500

    provide an immeditate fix for users experiencing issues with the incorrrect private subnet_range. This will allow user to specify their own without breaking the existing deployments.

--- variables.tf
+++ variables.tf
@@ -218,5 +218,5 @@ variable "cluster_name_random_string" {
 
 variable "subnet_range" {
   description = "Private IP space to be used in CIDR format"
-  default     = "172.16.0.0/16"
+  default     = "172.12.0.0/16"
 }
