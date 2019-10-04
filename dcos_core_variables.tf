d948eefc7f17261ce0541f794ce83f71370d97b9
Jan Ulferts <julferts@mesosphere.com>
2019-10-04 08:58:55 +0000

    CI - [ci-skip] - updated files

--- dcos_core_variables.tf
+++ dcos_core_variables.tf
@@ -16,7 +16,7 @@ variable "dcos_install_mode" {
 
 variable "dcos_version" {
   default     = "1.11.4"
-  description = "Specifies which DC/OS version instruction to use. Options: 1.13.3, 1.12.4, 1.11.10, etc. See dcos_download_path or dcos_version tree for a full list."
+  description = "Specifies which DC/OS version instruction to use. Options: 1.13.5, 1.12.4, 1.11.11, etc. See dcos_download_path or dcos_version tree for a full list."
 }
 
 # variable "role" {
