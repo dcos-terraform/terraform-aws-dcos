6a120b46bc5ca60e28fec789f380d02b37781366
Jan Ulferts <julferts@mesosphere.com>
2020-07-08 16:00:57 +0200

    fix: use 0.3.1 of install module

--- main.tf
+++ main.tf
@@ -179,7 +179,7 @@ module "dcos-infrastructure" {
 
 module "dcos-install" {
   source  = "dcos-terraform/dcos-install-remote-exec/null"
-  version = "~> 0.3.0"
+  version = "~> 0.3.1"
 
   # ansible related config
   ansible_bundled_container = var.ansible_bundled_container
